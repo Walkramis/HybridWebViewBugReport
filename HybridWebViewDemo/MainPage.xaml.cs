ff6721bb25d4366346b5ff2ec8e7c08cacc8795f
Walkramis <Andreas.hoglind@gmail.com>
2025-09-04 20:47:00 +0200

    Add files via upload

---
+++ HybridWebViewDemo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace HybridWebViewDemo;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+        hybridWebView.SetInvokeJavaScriptTarget(this);
+    }
+
+    private async void OnInvokeJSMethodButtonClicked(object sender, EventArgs e)
+    {
+        hybridWebView.EvaluateJavaScriptAsync("window.location = 'https://0.0.0.1/index.html#1234';");
+    }
+
+    private async void OnInvokeAsyncJSMethodButtonClicked(object sender, EventArgs e)
+    {
+        hybridWebView.EvaluateJavaScriptAsync("window.location = 'https://0.0.0.1/index.html?1234';");
+    }
+
+    private void hybridWebView_RawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
+    {
+        Dispatcher.Dispatch(() => editor.Text += Environment.NewLine + e.Message);
+    }
+}
